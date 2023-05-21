b6f1402176cc85af3180a73be2087c498c8e90a2
sumanth-coinflow <sumanth@coinflow.com>
2023-05-21 12:44:26 +0530

    update config

--- src/main/java/com/paymennt/solanaj/api/rpc/types/RpcTransactionConfig.java
+++ src/main/java/com/paymennt/solanaj/api/rpc/types/RpcTransactionConfig.java
@@ -1,55 +1,16 @@
 /************************************************************************ 
  * Copyright PointCheckout, Ltd.
- * 
+ *
  */
 package com.paymennt.solanaj.api.rpc.types;
 
+import lombok.Data;
 
-/**
- * 
- */
+@Data
 public class RpcTransactionConfig {
-
-    /**  */
     private SolanaCommitment commitment;
     private String encoding;
-    private String maxSupportedTransactionVersion;
+    private Integer maxSupportedTransactionVersion;
     private String transactionDetails;
     private String rewards;
-
-    public RpcTransactionConfig(SolanaCommitment commitment, String encoding, String maxSupportedTransactionVersion, String transactionDetails, String rewards) {
-        this.commitment = commitment;
-        this.encoding = encoding;
-        this.maxSupportedTransactionVersion = maxSupportedTransactionVersion;
-        this.transactionDetails = transactionDetails;
-        this.rewards = rewards;
-    }
-
-    /**
-     *
-     *
-     * @param commitment
-     */
-    public RpcTransactionConfig(SolanaCommitment commitment) {
-        this.commitment = commitment;
-    }
-
-    /**
-     * 
-     *
-     * @return 
-     */
-    public SolanaCommitment getCommitment() {
-        return commitment;
-    }
-
-    /**
-     * 
-     *
-     * @param commitment 
-     */
-    public void setCommitment(SolanaCommitment commitment) {
-        this.commitment = commitment;
-    }
-
 }
