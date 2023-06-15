c5af350134a4b395414d48a4dd889e9551fd030f
sumanth-coinflow <sumanth@coinflow.com>
2023-06-15 14:50:03 +0530

    add getters

--- src/main/java/com/paymennt/solanaj/api/rpc/types/RpcTokenAccounts.java
+++ src/main/java/com/paymennt/solanaj/api/rpc/types/RpcTokenAccounts.java
@@ -6,12 +6,14 @@ package com.paymennt.solanaj.api.rpc.types;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
 public class RpcTokenAccounts extends RpcResultObject {
 
     @JsonProperty("value")
@@ -19,6 +21,7 @@ public class RpcTokenAccounts extends RpcResultObject {
 
     @NoArgsConstructor
     @JsonIgnoreProperties(ignoreUnknown = true)
+    @Getter
     public static class TokenAccount {
         @JsonProperty("pubkey")
         private String publicKey;
@@ -27,23 +30,27 @@ public class RpcTokenAccounts extends RpcResultObject {
 
         @NoArgsConstructor
         @JsonIgnoreProperties(ignoreUnknown = true)
+        @Getter
         public static class AccountInfo {
             @JsonProperty("data")
             private Data data;
 
             @NoArgsConstructor
             @JsonIgnoreProperties(ignoreUnknown = true)
+            @Getter
             public static class Data {
                 @JsonProperty("parsed")
                 private ParsedData parsedData;
 
                 @NoArgsConstructor
                 @JsonIgnoreProperties(ignoreUnknown = true)
+                @Getter
                 public static class ParsedData {
                     @JsonProperty("info")
                     private AccountInfoData infoData;
 
                     @JsonIgnoreProperties(ignoreUnknown = true)
+                    @Getter
                     public static class AccountInfoData {
                         @JsonProperty("tokenAmount")
                         private TokenAmount tokenAmount;
