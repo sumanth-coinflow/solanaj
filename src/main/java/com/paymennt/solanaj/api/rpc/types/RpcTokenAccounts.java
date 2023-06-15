46542bfce8adbfba87cdc8d2b1c5fbe166231a6c
sumanth-coinflow <sumanth@coinflow.com>
2023-06-15 14:59:45 +0530

    add getters

--- src/main/java/com/paymennt/solanaj/api/rpc/types/RpcTokenAccounts.java
+++ src/main/java/com/paymennt/solanaj/api/rpc/types/RpcTokenAccounts.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 @NoArgsConstructor
